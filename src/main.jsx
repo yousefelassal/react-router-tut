0e39bab0977d21a6808fbc8e2bd3a16c485580fd
Yousef <yousefsameh24@gmail.com>
2023-07-02 21:23:11 +0300

    create contact route

--- src/main.jsx
+++ src/main.jsx
@@ -8,13 +8,18 @@ import './index.css'
 
 import Root from './routes/root'
 import ErrorPage from './error-page'
+import Contact from './routes/contact'
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
     errorElement: <ErrorPage />
-  }
+  },
+  {
+    path: 'contacts/:contactId',
+    element: <Contact />,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
