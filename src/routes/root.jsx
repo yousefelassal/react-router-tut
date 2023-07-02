5af1d63ed5ca9ad8d8d56a04692fe5182f13a894
Yousef <yousefsameh24@gmail.com>
2023-07-03 02:22:25 +0300

    submit form on change

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -5,12 +5,16 @@ import { Outlet,
     Form,
     redirect,
     useNavigation,
+    useSubmit,
     } from 'react-router-dom';
 import { getContacts, createContact } from '../contacts';
+import {useEffect} from 'react';
 
-export async function loader(){
-    const contacts = await getContacts();
-    return { contacts };
+export async function loader({ request }){
+    const url = new URL(request.url);
+    const q = url.searchParams.get("q");
+    const contacts = await getContacts(q);
+    return { contacts, q };
 }
 
 export async function action(){
@@ -19,20 +23,30 @@ export async function action(){
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
+
     return (
       <>
         <div id="sidebar">
           <h1>React Router Contacts</h1>
           <div>
-            <form id="search-form" role="search">
+            <Form id="search-form" role="search">
               <input
                 id="q"
                 aria-label="Search contacts"
                 placeholder="Search"
                 type="search"
                 name="q"
+                defaultValue={q}
+                onChange={(event) => {
+                    submit(event.currentTarget.form);
+                }}
               />
               <div
                 id="search-spinner"
@@ -43,7 +57,7 @@ export default function Root() {
                 className="sr-only"
                 aria-live="polite"
               ></div>
-            </form>
+            </Form>
             <Form method="post">
               <button type="submit">New</button>
             </Form>
